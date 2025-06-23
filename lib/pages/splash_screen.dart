ce25e3998caf7bde30d3f32a400b503b9bda03a6
Aryan <pakhrinp242@gmail.com>
2025-06-23 23:09:11 +0545

    splash_screen

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:cafe/pages/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Column(
+        children: [
+          SizedBox(
+            height: 60,
+            width: 60,
+            child: Lottie.asset("assets/animation/loading.json"),
+          ),
+          SizedBox(
+              // height: 20,
+              ),
+          Text(
+            "Have a Caffe",
+            style: TextStyle(
+              fontFamily: 'mono',
+              fontSize: 14,
+            ),
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      splashTransition: SplashTransition.fadeTransition,
+      duration: 4000,
+      nextScreen: LoginPage(),
+    );
+  }
+}
